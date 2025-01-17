f4197d808200408729ec8ada3c246eff44a023c3
Mlackofaca <mladen.milenkovic.5670@metropolitan.ac.rs>
2025-01-17 13:42:53 +0100

    Dodavanje pocetne

---
+++ src/components/Pocetna.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Pocetna() {
+  return (
+    <div>
+      <h1>Dobrodošli u aplikaciju za naručivanje hrane</h1>
+      <p>Pregledajte naš meni i naručite omiljenu hranu!</p>
+    </div>
+  );
+}
